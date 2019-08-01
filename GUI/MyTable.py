f3b9773fdf0ac6e01ddd11e39f0d58397d3e383e
duanhui <duanhui@duanhuideMacBook-Pro.local>
2019-08-01 19:53:35 +0800

    add some code

--- GUI/MyTable.py
+++ GUI/MyTable.py
@@ -2,19 +2,20 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+
 class MyTable(QWidget):
     def __init__(self):
         super(MyTable, self).__init__()
-        self._cost_type = ['产量', '员工工资']
+        self._cost_type = ['产量', '员工工资', 'a', 'b', 'c']
         self.initUI()
 
     def initUI(self):
-        self.resize(1000, 600)
-        conLayout = QHBoxLayout()
+        self.resize(900, 600)
+        conLayout = QVBoxLayout()
+
         tableWidget = QTableWidget()
         tableWidget.setRowCount(93)
         tableWidget.setColumnCount(26)
-        conLayout.addWidget(tableWidget)
         tableWidget.setHorizontalHeaderLabels([chr(i) for i in range(65, 65+26)])
 
         for i in range(12):
@@ -22,17 +23,25 @@ class MyTable(QWidget):
             tableWidget.setSpan(2, 2*i+1, 1, 2)
             tableWidget.setItem(0, 2*i+1, self.setItem('%d月实际' % (i+1)))
 
-            item1 = QTableWidgetItem('金额')
-            item2 = QTableWidgetItem('吨耗')
-
-            tableWidget.setItem(1, 2*i+1, item1)
-            tableWidget.setItem(1, 2*i+2, item2)
+            tableWidget.setItem(1, 2*i+1, self.setItem('金额'))
+            tableWidget.setItem(1, 2*i+2, self.setItem('吨耗'))
 
         for i in range(len(self._cost_type)):
-            item1 = QTableWidgetItem(self._cost_type[i])
-            tableWidget.setItem(2+i, 0, item1)
+            tableWidget.setItem(2+i, 0, self.setItem(self._cost_type[i]))
+
+        # twheader = QTableWidget()
+        # twheader.setRowCount(2)
+        # twheader.setColumnCount(26)
+        # twheader.horizontalHeader().setVisible(False)
+        # twheader.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        # twheader.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        # twheader.setFocusPolicy(Qt.NoFocus)
+        #
+        # conLayout.addWidget(twheader)
+        conLayout.addWidget(tableWidget)
 
         self.setLayout(conLayout)
+
         self.center()
 
     def center(self):
@@ -43,7 +52,9 @@ class MyTable(QWidget):
     def setItem(self,text):
         item = QTableWidgetItem(text)
         item.setTextAlignment(Qt.AlignCenter)
+
         return item
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
